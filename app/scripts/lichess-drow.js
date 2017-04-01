f12d5491649656fda156b4ddefefbcce1419604c
Vincent Simard <vincent@vincentsimard.com>
2017-04-01 16:20:29 -0400

    Drow?

--- app/scripts/lichess-drow.js
+++ app/scripts/lichess-drow.js
@@ -1,16 +1,5 @@
 'use strict';
 
-var lichess = {};
-
-lichess.el = document.querySelector('#lichess');
-lichess.elTable = lichess.el.querySelector('.lichess_ground .table');
-lichess.elBoard = lichess.el.querySelector('.lichess_board');
-lichess.elPlayerClock = lichess.el.querySelector('.clock_bottom');
-
-lichess.isGameOver = function() {
-  return this.elTable.classList.contains('finished');
-};
-
 var makeAudio = function(file, volume) {
   var audio = new Audio(chrome.extension.getURL('ogg/' + file));
   audio.volume = volume;
@@ -18,33 +7,26 @@ var makeAudio = function(file, volume) {
   return audio;
 };
 
-var timeWarning = function() {
-  var audio = makeAudio('Lowtime.wav', 1);
+var droooow = function() {
+  var audio = makeAudio('drow.wav', 1);
   audio.play();
-
-  timeWarning = function() {};
 };
 
 var handleMutation = function(mutations) {
-  var firstTarget = mutations[0].target;
-  var classes = firstTarget.classList;
+  var addedNodes = mutations[0].addedNodes;
 
-  var hasEmerg = classes.contains('emerg');
-  var hasOutOfTime = classes.contains('outoftime');
-  
-  if (hasEmerg && !hasOutOfTime && !lichess.isGameOver()) {
-    timeWarning();
+  if (addedNodes.length > 0 && addedNodes[0].innerText.includes('offers draw')) {
+    droooow();
   }
 };
 
 var init = function() {
-  // @TODO: Only play sound for games being played (not spectating)
-  if (!lichess.elPlayerClock) { return; }
+  var drowObserver = new MutationObserver(handleMutation);
+  var config = { childList: true };
+  var elChat = document.querySelector('.discussion .messages');
 
-  var emergObserver = new MutationObserver(handleMutation);
-  var config = { attributeFilter: ['class'] };
-  
-  emergObserver.observe(lichess.elPlayerClock, config);
+  drowObserver.observe(elChat, config);
 };
 
-init();
+// Blah
+window.setTimeout(init, 1000);
